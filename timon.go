1e356c8f1e1943c06ef272f94ff58971959484e3
vabbb <valerio.baudo96@gmail.com>
2019-05-20 23:01:10 +0200

    added filter, packet processing is now faster, printing separate parts of each packet

--- timon.go
+++ timon.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"os"
 	"time"
+	"unicode"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
@@ -16,7 +18,18 @@ var (
 	err         error
 	timeout     = 100 * time.Millisecond
 	handle      *pcap.Handle
+	ethLayer    layers.Ethernet
+	ipLayer     layers.IPv4
+	tcpLayer    layers.TCP
 )
+
+//IsASCIIPrintable => {true if char is printable ; false otherwise}
+func IsASCIIPrintable(r rune) bool {
+	if r > unicode.MaxASCII || !unicode.IsPrint(r) {
+		return false
+	}
+	return true
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -34,18 +47,59 @@ func main() {
 	}
 	defer handle.Close()
 
-	// Set filter
+	// Set filter for only tcp traffic. Can also filter port #s with this
 	var filter = "tcp"
 	err = handle.SetBPFFilter(filter)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Only capturing TCP.")
 
 	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	//go through all the packages being received
 	for packet := range packetSource.Packets() {
-		// Process packet here
-		fmt.Println(packet)
+		parser := gopacket.NewDecodingLayerParser(
+			layers.LayerTypeEthernet,
+			&ethLayer,
+			&ipLayer,
+			&tcpLayer,
+		)
+
+		//divide packet into layers
+		foundLayerTypes := []gopacket.LayerType{}
+		err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+		if err != nil {
+			fmt.Println("Trouble decoding layers: ", err)
+		}
+
+		//go through packet layers
+		for _, layerType := range foundLayerTypes {
+			//print IP info
+			if layerType == layers.LayerTypeIPv4 {
+				fmt.Println("IPv4: ", ipLayer.SrcIP, "->", ipLayer.DstIP)
+			}
+			//print TCP info
+			if layerType == layers.LayerTypeTCP {
+				fmt.Println("TCP Port: ", tcpLayer.SrcPort, "->", tcpLayer.DstPort)
+				fmt.Println("TCP SYN:", tcpLayer.SYN, " | ACK:", tcpLayer.ACK)
+
+				//go through all bytes in the tcp payload
+				for i, octet := range tcpLayer.Payload {
+					//if character is printable, print it; else print a "."
+					if IsASCIIPrintable(rune(octet)) {
+						fmt.Print(string(octet), " ")
+					} else {
+						fmt.Print(". ")
+					}
+					//every 40 characters printed, print a newline
+					if (i+1)%40 == 0 {
+						fmt.Println()
+					}
+				}
+
+				fmt.Print("\n-------------------------------\n")
+			}
+		}
 	}
 }
