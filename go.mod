f58d85889e3105b600928010d4517be7cbfd9b3b
vabbb <valerio.baudo96@gmail.com>
2019-05-20 10:17:43 +0200

    First commit!

---
+++ go.mod
@@ -0,0 +1,5 @@
+module timon
+
+go 1.12
+
+require github.com/google/gopacket v1.1.17
---
+++ timon.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	device            = "wlp3s0"
+	snapshotLen int32 = 1024
+	promiscuous       = false
+	err         error
+	timeout     time.Duration = 5 * time.Second
+	handle      *pcap.Handle
+)
+
+func main() {
+
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	// Use the handle as a packet source to process all packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		// Process packet here
+		fmt.Println(packet)
+	}
+}
