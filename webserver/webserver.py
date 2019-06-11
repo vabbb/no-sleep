5a1a5c819de906ba58b6e94e95133f43a8504232
Gianmaria Del Monte <g.macmount@gmail.com>
2019-06-11 20:33:38 +0200

    html: added jinja macroes for starred flows and all flows

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import fake_db as db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello world!"
+    starred = db.get_starred()
+    flows   = db.get_flow_list()
+    return render_template('index.html', starred=starred, flows=flows)
+
+@app.route("/star/<int:flow_id>/<sel>", methods=['POST'])
+def star(flow_id, sel):
+    db.star_flow(flow_id, sel)
+    return "ok"
 
