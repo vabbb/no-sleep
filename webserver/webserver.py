46f5a93fe8adb2fbcf2b3b63bfd58ceae7deb352
Gianmaria Del Monte <g.macmount@gmail.com>
2019-06-11 21:48:05 +0200

    web: added update page on starred flows

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import fake_db as db
+from pprint import pprint
 
 app = Flask(__name__)
 
@@ -12,5 +13,10 @@ def hello_world():
 @app.route("/star/<int:flow_id>/<sel>", methods=['POST'])
 def star(flow_id, sel):
     db.star_flow(flow_id, sel)
+    pprint(db.get_flow_list())
     return "ok"
 
+@app.route("/starred", methods=['POST'])
+def starred():
+    starred = db.get_starred()
+    return render_template('starred.html', starred=starred)
