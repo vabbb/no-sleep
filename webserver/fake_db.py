24f86ec92248d93d5a78f0425a010eca95edde63
Gianmaria Del Monte <g.macmount@gmail.com>
2019-06-11 17:22:42 +0200

    db: fake db interface

---
+++ webserver/fake_db.py
@@ -0,0 +1,28 @@
+from flaw_examples import *
+
+def get_flow_list():
+    return flows
+
+def get_flow(id):
+    for flow in flows:
+        if flow['id'] == id:
+            return flow
+    return False
+
+def get_flow_data(id):
+    f = get_flow(id)
+    if (f):
+        return f['dataFlow']
+    return False
+
+def get_starred():
+    res = []
+    for flow in flows:
+        if flow['favourite']:
+            res.append(flow)
+    return res
+
+def star_flow(id):
+    f = get_flow(id)
+    if (f):
+        f['favourite'] = True
---
+++ webserver/flaw_examples.py
@@ -0,0 +1,40 @@
+flows = [
+    {
+        "id" : 1,
+        "srcIP" : "1.1.1.1",
+        "dstIP" : "1.1.1.2",
+        "srcPort"   : 1234,
+        "dstPort"   : 4321,
+        "time"  : 1,
+        "hasFlag" : False,
+        "favourite" : False,
+        "hasSYN" : False,
+        "hasFIN" : False,
+        "dataFlow" : {
+            "size" : 10,
+            "data" : "Hello world!1",
+            "hex"  : "1238128397"
+        }
+
+    },
+
+    {
+        "id" : 2,
+        "srcIP" : "1.1.1.1",
+        "dstIP" : "1.1.1.2",
+        "srcPort"   : 1234,
+        "dstPort"   : 4321,
+        "time"  : 1,
+        "hasFlag" : False,
+        "favourite" : False,
+        "hasSYN" : False,
+        "hasFIN" : False,
+        "dataFlow" : {
+            "size" : 10,
+            "data" : "Hello world!2",
+            "hex"  : "1238128397"
+        }
+
+    }
+
+]
