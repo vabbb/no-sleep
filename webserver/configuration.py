5117655579f7d1c94592c21ec0ad29e37b509ea3
Gianmaria Del Monte <g.macmount@gmail.com>
2019-06-13 19:11:56 +0200

    server: changed services configuration

--- webserver/configuration.py
+++ webserver/configuration.py
@@ -1,7 +1,7 @@
 vm_ip = "2.2.2.2"
 
-services = [{"ip": vm_ip, "port": 9876, "name": "timon"},
-            {"ip": vm_ip, "port": 80, "name": "beep beep"},
-            {"ip": vm_ip, "port": 8080, "name": "mufasa"},
-            {"ip": vm_ip, "port": 5000, "name": "simba"},
-            {"ip": vm_ip, "port": 1883, "name": "pumbaa"}]
+services = {9876: "timon",
+            80: "beep beep",
+            443: "mufasa",
+            5000: "simba",
+            40510: "pumbaa"}
