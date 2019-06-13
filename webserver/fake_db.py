eaeba62915bc010dc0abd15a0e62e8fa48bd1ca3
Gianmaria Del Monte <g.macmount@gmail.com>
2019-06-13 11:39:10 +0200

    db: update examples

--- webserver/fake_db.py
+++ webserver/fake_db.py
@@ -1,28 +1,50 @@
 from flaw_examples import *
+from pprint import pprint
 
 def get_flow_list():
-    return flows
+    return connections
 
-def get_flow(id):
-    for flow in flows:
-        if flow['id'] == id:
-            return flow
+
+def get_connection(id):
+    for c in connections:
+        if c['connID'] == str(id):
+            return c
     return False
+
+
+def get_info_connection(id):
+    c = get_connection(id)
+    if c:
+        res = {}
+        res['connID'] = c['connID']
+        res['srcIP'] = c['srcIP']
+        res['dstIP'] = c['dstIP']
+        res['srcPort'] = c['srcPort']
+        res['dstPort'] = c['dstPort']
+        res['lastSeen'] = c['lastSeen']
+        res['favorite'] = c['favorite']
+
+        return res
+
+    return False
+
 
 def get_flow_data(id):
-    f = get_flow(id)
-    if (f):
-        return f['dataFlow']
+    f = get_connection(id)
+    if f:
+        return f['flows']
     return False
+
 
 def get_starred():
     res = []
-    for flow in flows:
-        if flow['favorite']:
-            res.append(flow)
+    for c in connections:
+        if c['favorite']:
+            res.append(c)
     return res
 
+
 def star_flow(id, sel):
-    f = get_flow(id)
-    if (f):
-        f['favorite'] = True if sel == "true" else False
+    c = get_connection(id)
+    if c:
+        c['favorite'] = True if sel == "true" else False
--- webserver/flaw_examples.py
+++ webserver/flaw_examples.py
@@ -1,40 +1,195 @@
-flows = [
+connections = [
     {
-        "id" : 1,
-        "srcIP" : "1.1.1.1",
-        "dstIP" : "1.1.1.2",
-        "srcPort"   : 1234,
-        "dstPort"   : 4321,
-        "time"  : 12349872394908,
-        "hasFlag" : False,
-        "favorite" : True,
-        "hasSYN" : False,
-        "hasFIN" : False,
-        "dataFlow" : {
-            "size" : 10,
-            "data" : "Hello world!1",
-            "hex"  : "1238128397"
-        }
+        "connID": "1",
+        "srcIP": "1.1.1.1",
+        "dstIP": "2.2.2.2",
+        "srcPort": 1234,
+        "dstPort": 80,
+        "lastSeen": 123456789,
+        "favorite": False,
+        "flows":   [
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Hello world!",
+                "hex": "asdjkhasjh"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "2.2.2.2:80",
+                "dst": "1.1.1.1:1234",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Hi",
+                "hex": "asdas"
+            }
 
+        ]
     },
 
     {
-        "id" : 2,
-        "srcIP" : "1.1.1.1",
-        "dstIP" : "1.1.1.2",
-        "srcPort"   : 1234,
-        "dstPort"   : 4321,
-        "time"  : 128347298347923,
-        "hasFlag" : False,
-        "favorite" : False,
-        "hasSYN" : False,
-        "hasFIN" : False,
-        "dataFlow" : {
-            "size" : 10,
-            "data" : "Hello world!2",
-            "hex"  : "1238128397"
-        }
+        "connID": "2",
+        "srcIP": "1.1.1.1",
+        "dstIP": "2.2.2.2",
+        "srcPort": 1234,
+        "dstPort": 80,
+        "lastSeen": 123456789,
+        "favorite": False,
+        "flows":   [
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Ciao maurizio",
+                "hex": "asdjkhasjh"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "2.2.2.2:80",
+                "dst": "1.1.1.1:1234",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Ciao Antonio",
+                "hex": "asdas"
+            }, {
 
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Quanti anni hai?",
+                "hex": "asdjkhasjh"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "2.2.2.2:80",
+                "dst": "1.1.1.1:1234",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Io 33 e tu?",
+                "hex": "asdas"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "24",
+                "hex": "asdjkhasjh"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "2.2.2.2:80",
+                "dst": "1.1.1.1:1234",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Bravo",
+                "hex": "asdas"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Che hai mangiato a pranzo?",
+                "hex": "asdjkhasjh"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "2.2.2.2:80",
+                "dst": "1.1.1.1:1234",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "La pasta con le telline",
+                "hex": "asdas"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Buona!",
+                "hex": "asdjkhasjh"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "2.2.2.2:80",
+                "dst": "1.1.1.1:1234",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Vabbè io vado, ciao",
+                "hex": "asdas"
+            },
+            {
+                "flowID": "1",
+                "connID": "1",
+                "src": "1.1.1.1:1234",
+                "dst": "2.2.2.2:80",
+                "time": 8754878687,
+                "favorite": False,
+                "hasSYN": False,
+                "hasFIN": False,
+                "size": 10,
+                "data": "Ciao.",
+                "hex": "asdjkhasjh"
+            }
+        ]
     }
-
 ]
