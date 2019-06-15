b4ecf3a201c2bc866c5787ec2a80baf7268d09ce
Valerio Baudo <valerio.baudo96@gmail.com>
2019-06-15 09:51:54 +0200

    tcpflow package

--- tcpflow_parser/main.go
+++ tcpflow_parser/main.go
@@ -1,4 +1,15 @@
 package main
 
+type nodet struct {
+	connID           string
+	srcIP, dstIP     string
+	srcPort, dstPort uint16
+	hasFlag          bool
+	time             int64
+	data             string
+	hex              []byte
+}
+
 func main() {
+
 }
